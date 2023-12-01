70e5b682b4b057a5237667dc865f899a56c89ec8
massima <mastem@msn.com>
2023-12-01 11:39:59 +0100

    feat : create prisma service using nest cli command

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ProductModule],
   controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  providers: [AppService, ProductService, PrismaService],
 })
 export class AppModule {}
